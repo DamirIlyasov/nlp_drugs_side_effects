661cd5335c419a06bea1f8a5e08f0b8d478788e8
DamirIlyasov <damirilyasov1997@gmail.com>
2018-05-17 11:43:17 +0300

    refactored

--- F1.py
+++ F1.py
@@ -1,14 +1,18 @@
 import sys
 
 for argument in sys.argv:
-    nextArgument = sys.argv[sys.argv.index(argument) + 1]
+    nextArgument = None
+    if sys.argv.index(argument) < (len(sys.argv) - 1):
+        nextArgument = sys.argv[sys.argv.index(argument) + 1]
     if argument == '--src-train-texts':
         # путь к корпусу, обязательный аргумент
         pathToCorpus = nextArgument
+        print('pathToCorpus:', pathToCorpus)
 
     if argument == '--text-encoding':
         # кодировка-текста в файлах корпуса
         textEncoding = nextArgument
+        print('textEncoding:', textEncoding)
 
     if argument == '--word-type':
         # возможные значения:
@@ -18,20 +22,25 @@ for argument in sys.argv:
         # в случае stem – “стемма” (см. http://snowball.tartarus.org/ ),
         # в случае suffix_X – окончания слов длиной X
         wordType = nextArgument
+        print('wordType:', wordType)
 
     if argument == '-n':
         # n-грамность
         grammar = nextArgument
+        print('grammar:', grammar)
 
     if argument == '--features':
         #   <false|true>
         # использовать дополнительные hand-crafted признаки, указанные в задании
         features = nextArgument
+        print('features:', features)
 
     if argument == '--unknown-word-freq':
         # частота, ниже которой слова в обуч. множестве считаются неизвестными
         unknownWordFrequency = nextArgument
+        print('unknownWordFrequency:', unknownWordFrequency)
 
     if argument == '-o':
         # путь-куда-сохранить-сериализованную-языковую-модель, обязательный аргумент
         pathToSerializedModel = nextArgument
+        print('pathToSerializedModel:', pathToSerializedModel)
--- F3.py
+++ F3.py
@@ -1,15 +1,21 @@
 import sys
 
 for argument in sys.argv:
-    nextArgument = sys.argv[sys.argv.index(argument) + 1]
+    nextArgument = None
+    if sys.argv.index(argument) < (len(sys.argv) - 1):
+        nextArgument = sys.argv[sys.argv.index(argument) + 1]
+
     if argument == '--lm':
         # путь-к-сериализованной-модели
         pathToSerializedModel = nextArgument
+        print('pathToSerializedModel:', pathToSerializedModel)
 
     if argument == '--src-texts':
         # путь-к-коллекции
         textPath = nextArgument
+        print('textPath:', textPath)
 
     if argument == '--o-texts':
         # путь-куда-сохранить-размеченную-коллекцию
         pathToSave = nextArgument
+        print('pathToSave:', pathToSave)
